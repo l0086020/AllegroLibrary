1fe90829d39d52f9c942cbcbbe8bc1821969336e
XTAY <mr_zh-ang@qq.com>
2024-10-09 20:50:11 +0800

    1、修改快捷键打开脚本 2、修改部分原理图库、PCB库

--- Shortcut Keys/打开env文件.bat
+++ Shortcut Keys/打开env文件.bat
@@ -1,1 +1,19 @@
-explorer c:\Cadence\SPB_17.4\share\pcb\text\
+for %%d in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+    if exist "%%d:\Program Files\Cadence\share\pcb\text" (
+        start explorer "%%d:\Program Files\Cadence\share\pcb\text"
+        set found=1
+        goto search
+    )
+)
+
+for %%d in (C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
+    for /d %%p in (%%d:\*) do (
+        if /i "%%~npx"=="Cadence" (
+            start explorer "%%p\share\pcb\text"
+            set found=1
+            goto search
+        )
+    )
+)
+
+:end
